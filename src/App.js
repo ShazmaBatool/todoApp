b544cfe602bf949c35f7e0f3f76ece069b20711e
ShazmaBatool <shazmabatool12edu@gmail.com>
2023-11-07 15:00:54 +0500

    Version 1 with functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get("/api/tasks");
+      setTasks(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addTask = async () => {
+    if (newTask.trim() === "") return;
+    try {
+      await axios.post("/api/tasks", { title: newTask });
+      setNewTask("");
+      fetchTasks();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteTask = async (taskId) => {
+    try {
+      await axios.delete(`/api/tasks/${taskId}`);
+      fetchTasks();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <h1>Todo List</h1>
+          <Form>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="New task"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={addTask}>
+              Add Task
+            </Button>
+          </Form>
+          <ul>
+            {tasks.map((task) => (
+              <li key={task.id}>
+                {task.title}
+                <Button variant="danger" onClick={() => deleteTask(task.id)}>
+                  Delete
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
